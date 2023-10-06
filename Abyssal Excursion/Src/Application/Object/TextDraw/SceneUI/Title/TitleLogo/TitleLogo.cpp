d24a90a4236257364e59d8d343d40b3b46da10ef
1205041 <kd1336933@st.kobedenshi.ac.jp>
2023-10-06 14:00:40 +0900

    AE1006-1
    タイトルロゴを変更した

--- Abyssal Excursion/Src/Application/Object/TextDraw/SceneUI/Title/TitleLogo/TitleLogo.cpp
+++ Abyssal Excursion/Src/Application/Object/TextDraw/SceneUI/Title/TitleLogo/TitleLogo.cpp
@@ -6,8 +6,8 @@ void TitleLogo::Update()
 
 void TitleLogo::Init()
 {
-	SetText("Asset/Textures/SceneUI/Title/TitleLogo.png");
-	SetRcAngle({ 0, 0, 600, 100 });
+	SetText("Asset/Textures/SceneUI/Title/TitleLg.png");
+	SetRcAngle({ 0, 0, 700, 100 });
 	SetColor({ 1.0f,1.0f,1.0f,1.0f });
 
 	m_texPos = Math::Vector3::Zero;
