63fdada626440689c2bdd8994acf81f2a907510a
1205041 <kd1336933@st.kobedenshi.ac.jp>
2023-10-12 09:01:18 +0900

    AF1012-1
    中途発表会本番

--- Archer Fish/Src/Application/Object/TextDraw/SceneUI/Title/Enter/Enter.h
+++ Archer Fish/Src/Application/Object/TextDraw/SceneUI/Title/Enter/Enter.h
@@ -9,7 +9,7 @@ public:
 
 	void Update()		override;
 	void PostUpdate()	override;
-	void DrawSprite()	override;
+//	void DrawSprite()	override;
 	void Init()			override;
 private:
 
@@ -17,9 +17,9 @@ private:
 	float m_colorC = 0.1f;
 
 	// éläpÇÃïœêî
-	Math::Vector2	m_pos	= Math::Vector2::Zero;
-	Math::Vector2	m_half	= Math::Vector2::Zero;
-	Math::Color		m_col	= { 1.0f,1.0f,1.0f,1.0f };
+//	Math::Vector2	m_pos	= Math::Vector2::Zero;
+//	Math::Vector2	m_half	= Math::Vector2::Zero;
+//	Math::Color		m_col	= { 1.0f,1.0f,1.0f,1.0f };
 
-	POINT m_point;
+//	POINT m_point;
 };
