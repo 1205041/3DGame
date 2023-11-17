39f2b0d30ec5151aa367386e30b6617dc37296db
1205041 <kd1336933@st.kobedenshi.ac.jp>
2023-11-17 10:43:40 +0900

    AF1117-1
    ほとんど変化無し

--- ArcherFish/Src/Application/Scene/TitleScene/TitleScene.cpp
+++ ArcherFish/Src/Application/Scene/TitleScene/TitleScene.cpp
@@ -21,18 +21,18 @@ void TitleScene::Init()
 //	KdAudioManager::Instance().Play("Asset/Sounds/BGM/Title.wav", true);
 	
 	// オブジェクトの初期化
-//	std::shared_ptr<TextDraw> spText;
-//	spText = std::make_shared<TextDraw>();
-//	spText->SetLoadText("Asset/Textures/BackDrop/BDimg.png", { 0.0f,0.0f }, { 0,0,1280,720 });
-//	m_objList.push_back(spText);
+	std::shared_ptr<TextDraw> spText;
+	spText = std::make_shared<TextDraw>();
+	spText->SetLoadText("Asset/Textures/BackDrop/BDimg.png", { 0.0f,0.0f }, { 0,0,1280,720 });
+	m_objList.push_back(spText);
 
-//	spText = std::make_shared<TextDraw>();
-//	spText->SetLoadText("Asset/Textures/SceneUI/Title/Logo/TitleLogo.png", { 0.0f,0.0f }, { 0,0,450, 90 });
-//	m_objList.push_back(spText);
+	spText = std::make_shared<TextDraw>();
+	spText->SetLoadText("Asset/Textures/SceneUI/Title/Logo/TitleLogo.png", { 0.0f,0.0f }, { 0,0,450, 90 });
+	m_objList.push_back(spText);
 
-//	std::shared_ptr<Enter> spEnter;
-//	spEnter = std::make_shared<Enter>();
-//	m_objList.push_back(spEnter);
+	std::shared_ptr<Enter> spEnter;
+	spEnter = std::make_shared<Enter>();
+	m_objList.push_back(spEnter);
 
 	std::shared_ptr<Bubble> spBublle;
 	spBublle = std::make_shared<Bubble>();
